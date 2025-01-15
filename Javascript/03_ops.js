62357bc3ca76d6dae7a27dca7cb2aa49f14e3d23
Yash Gupta <37302305+yashguptag@users.noreply.github.com>
2025-01-15 10:52:36 +0000

    operations

---
+++ Javascript/03_ops.js
@@ -0,0 +1,34 @@
+/* OPERATIONS */ 
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*8);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "yash"
+let str2 = " ofc he is a fkinn genius"
+
+let str3= str1 + str2
+//console.log(str3)
+
+//console.log("1" + 2)
+//console.log(2 + "1")
+//console.log("2" + 1 + 1)
+//console.log(2 + 1 + "1")
+
+// console .log(2 + 4 * 5 % 3) don't don't this type of shit in real world code, it won't get merged in big companies and might create problems later
+
+//console.log(+true);
+//console.log(+"");
+
+/* let num1, num2, num3 
+
+num1 = num2 = num3 = 2 + 2
+ again, don't try to be oversmart, focus more on readability*/
+
+ ++ precedence operator in prefix and postfix (understand this distinction, see on mdn)
