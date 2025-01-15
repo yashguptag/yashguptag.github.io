6eb2e994949fb2f55ed06ebdd2060868507d3135
Yash Gupta <37302305+yashguptag@users.noreply.github.com>
2025-01-15 06:31:51 +0000

    second update

---
+++ Javascript/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as a newer version
+
+// alert( 3 + 3 ) // we are using nodejs, not browser
+
+// console.log(3 + 3) console.log("Yash") this will give you a fucking error because of missing semi colon in first console, & anyway general readability of code should be high bro, not shooting from your ass.
+
+/* console.log(3 
+ +
+  3) don't do this bull behaviour */
+
+  let name = "yash"
+  let age  = 18
+  let isLoggedIn = false
+   
+
+  // Primitive Data Types---
+  // number , range is 2 to power 53
+  // bigint
+  // string, use in double quotes
+  // boolean, ha ya na
+  // null, standalone value bhi hai
+  // undefined 
+  // symbol, to identify the uniqueness of different components, used a lot in react and tools like figma
+
+  // Object---
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
--- Javascript/yash.js
+++ Javascript/yash.js
@@ -1,1 +1,10 @@
 console.log("Hitesh")
+const accountId = 144553
+let accountEmail = "yash@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+// accountId = 2 // not allowed
+console.log(accountId);
+/* Prefer not to use var, beacause of issue  in block scope and functional scope */
+console.table({accountId, accountPassword, accountCity, accountState})
